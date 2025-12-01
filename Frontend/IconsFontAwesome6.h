b7d9e1c53e5c93e4df9c1a08c4f6ce70ba8ff179
tiyun2012 <hoathinhvietnam2016@gmail.com>
2025-12-01 14:52:20 +0700

    first commit

---
+++ Frontend/IconsFontAwesome6.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#define FONT_ICON_FILE_NAME_FAR "fa-regular-400.ttf"
+#define FONT_ICON_FILE_NAME_FAS "fa-solid-900.ttf"
+#define FONT_ICON_FILE_NAME_FAB "fa-brands-400.ttf"
+
+#define ICON_MIN_FA 0xe005
+#define ICON_MAX_FA 0xf8ff
+#define ICON_MIN_FAR 0xf004
+#define ICON_MAX_FAR 0xf2eD
+#define ICON_MIN_FAB 0xe007
+#define ICON_MAX_FAB 0xf8e8
+
+#define ICON_FA_DOOR_OPEN "\xef\x94\xab"
+#define ICON_FA_MAGNIFYING_GLASS "\xef\x80\x82"
+#define ICON_FA_FLOPPY_DISK "\xef\x83\x87"
+#define ICON_FA_GAMEPAD "\xef\x84\x9b"
